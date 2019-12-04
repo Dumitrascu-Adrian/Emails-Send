84a994e52192baf2498a14f1909574e4dfe54698
Dumitrascu-Adrian <39406388+Dumitrascu-Adrian@users.noreply.github.com>
2019-12-04 14:51:51 +0200

    first commit

---
+++ EmailSend/EmailSend/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace EmailSend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Message = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Subject = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(140, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(236, 356);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Message
+            // 
+            this.Message.Location = new System.Drawing.Point(574, 102);
+            this.Message.Multiline = true;
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(450, 295);
+            this.Message.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(162, 423);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(265, 423);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 31);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Send";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(482, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Message";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(482, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Subject";
+            // 
+            // Subject
+            // 
+            this.Subject.Location = new System.Drawing.Point(574, 38);
+            this.Subject.Multiline = true;
+            this.Subject.Name = "Subject";
+            this.Subject.Size = new System.Drawing.Size(450, 37);
+            this.Subject.TabIndex = 7;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(574, 430);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(450, 23);
+            this.progressBar1.TabIndex = 8;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1102, 481);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Subject);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox Message;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Subject;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ EmailSend/EmailSend/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+namespace EmailSend
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+        string[] lines;
+        void ReadFile()
+        {
+            string input = "";
+
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Title = "C# Corner Open File Dialog";
+            fileDialog.InitialDirectory = @"D:\";
+            fileDialog.Filter = "All files (*.*)|*.*";
+            fileDialog.RestoreDirectory = true;
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                input = fileDialog.FileName;
+
+            }         
+            lines = System.IO.File.ReadAllLines(input);
+            foreach (string line in lines)
+            {
+                listBox1.Items.Add(line);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReadFile();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            progressBar1.Visible = true;
+            progressBar1.Value = 0;
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = lines.Length;
+            progressBar1.Step = 1;
+            try
+            { 
+                SmtpClient SmtpServer = new SmtpClient();
+                SmtpServer.Host = "smtp.gmail.com";
+                SmtpServer.Port = 587;
+
+                SmtpServer.EnableSsl = true;
+                SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+                SmtpServer.UseDefaultCredentials = false;
+                string username = ""; //gmailul tau
+                string password = "";  //parola contului
+                SmtpServer.Credentials = new System.Net.NetworkCredential(username, password);
+
+                foreach (string line in lines)
+                {
+                    MailMessage mail = new MailMessage();
+
+                    mail.From = new MailAddress(username);
+                    mail.To.Add(line);
+                    mail.Subject = Subject.Text;
+                    mail.Body = Message.Text;
+
+
+                    SmtpServer.Send(mail);
+                    progressBar1.PerformStep();
+
+
+                 }
+                progressBar1.Visible = false;
+                MessageBox.Show("mail Send");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+     
+    }
+}
